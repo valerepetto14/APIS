2f6dd1b9887d8862f58f5fae66e56e13bb9a9f84
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-30 09:51:20 -0300

    add more info

--- API imagenes/app.js
+++ API imagenes/app.js
@@ -1,16 +1,34 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });//indicamos el destino de donde se guardaran las imagenes
+const imageStorage = multer.diskStorage({
+    // Destination to store image     
+    destination: 'uploads', 
+      filename: (req, file, cb) => {
+          cb(null, file.fieldname + '_' + Date.now() 
+             + path.extname(file.originalname))
+            // file.fieldname is name of the field (image)
+            // path.extname get the uploaded file extension
+    }
+});
+
+const imageUpload = multer({
+    storage: imageStorage,
+    fileFilter(req, file, cb) {
+     cb(undefined, true)
+  }
+}) 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/files", upload.single("files"), (req, res) =>{
-    console.log(req.body);
-    console.log(req.files);
-    res.json({ message: "Successfully uploaded files" });
-});
+
+app.post('/file', imageUpload.single('image'), (req, res) => {
+    res.send(req.file)
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
 
 app.listen(3000, () =>{
     console.log("andandooo")
